e8b3a615b60f18f63cff35a334aacd424a462829
Ashwinumbarkar20 <Ashwinumbarkar20@gmail.com>
2024-01-16 16:39:51 +0530

    Navbar

---
+++ Scheduler/src/Component/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div className='contaoner-fluid'>
+      <div>Logo</div>
+      <div><ul>
+        <li>Item 1</li>
+        <li>Item 2</li>
+        <li>Item 3</li>
+        <li>Item 4</li>
+        <li>Item 5</li>
+      </ul></div>
+    </div>
+  )
+}
